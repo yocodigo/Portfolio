e9684c90982a117bb2c8addbf2bf78609bc6f969
Joel <contact_jhern@proton.me>
2025-03-26 11:21:50 -0700

    Add a contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,154 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+
+export default function Contact() {
+  return (
+    <ContainerBlock
+      title="Contact - Joel Hernandez"
+      description="Get in touch with me for collaboration or questions"
+    >
+      <section>
+        <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+          <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+            Contact
+          </h1>
+        </div>
+        <div className="relative z-10 rounded-md shadow-md bg-blue-600 p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="md:ml-4">
+              <header className="">
+                <h1 className="text-gray-50 font-semibold text-2xl">
+                  Get in touch, let's talk.
+                </h1>
+                <p className="font-light text-base text-gray-200 mt-2">
+                  Fill in the details and I'll get back to you as soon as I can.
+                </p>
+              </header>
+              <div className="icons-container inline-flex flex-col my-20">
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-blue-600 p-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-telephone-fill h-4 w-4 text-gray-50"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+                    />
+                  </svg>
+                  <p className="text-gray-50 font-light text-sm">
+                    +1 (123) 456-7890
+                  </p>
+                </div>
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-blue-600 p-4 mt-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-envelope-fill h-4 w-4 text-gray-50"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+                  </svg>
+                  <p className="text-gray-50 font-light text-sm">
+                    contact@yourwebsite.com
+                  </p>
+                </div>
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-blue-600 p-4 mt-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-pin-fill h-4 w-4 text-gray-50"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224.5-.5.5-.276 0-.5-.224-.5-.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
+                  </svg>
+                  <p className="text-gray-50 font-light text-sm">
+                    San Francisco, California
+                  </p>
+                </div>
+              </div>
+              <div className="social-icons flex flex-row space-x-8">
+                <a
+                  href="https://github.com/yocodigo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="h-10 w-10 rounded-full bg-white flex items-center justify-center hover:bg-blue-500 transition-colors duration-300"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    className="text-blue-600 hover:text-white transition-colors duration-300"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.923.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/joelhdev/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="h-10 w-10 rounded-full bg-white flex items-center justify-center hover:bg-blue-500 transition-colors duration-300"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    className="text-blue-600 hover:text-white transition-colors duration-300"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <form className="form rounded-lg bg-white p-4 flex flex-col">
+              <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+                Your Name
+              </label>
+              <input
+                type="text"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                name="name"
+              />
+              <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+                Email
+              </label>
+              <input
+                type="text"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                name="email"
+              />
+              <label
+                htmlFor="message"
+                className="text-sm text-gray-600 mx-4 mt-4"
+              >
+                Message
+              </label>
+              <textarea
+                rows="4"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                name="message"
+              ></textarea>
+              <button
+                type="submit"
+                className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </ContainerBlock>
+  );
+}
